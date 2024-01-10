20e3fa56563f43e7853db67b7975ba472eeeb981
Gustavo Canellas Aveline Rocha <125305894+GustavoCanellasAvelineRocha@users.noreply.github.com>
2024-01-10 16:40:18 -0300

    Script4

--- Aula03/script4.js
+++ Aula03/script4.js
@@ -1,2 +1,16 @@
-//Faça um algoritmo que imprima no console os valores como se fosse um 
+//Faça um algoritmo que imprima no console os valores como se fosse um
 //relógio durante 1 minuto, ou seja que fique exibindo a hora, os minutos e os segundos em tempo real;
+
+function printaMinutos() {
+  let seconds = 0;
+  const intervalo = setInterval(() => {
+    const data = new Date();
+    seconds++;
+    console.log("Horario atual = " +data.getHours() + ":" + data.getMinutes() + ":" + data.getSeconds());
+    if (seconds == 60) {
+      clearInterval(intervalo);
+    }
+  }, 1000);
+}
+
+printaMinutos();
