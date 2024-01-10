c9a0e884f6a47313ee51cf1d0af12ab2c839bd9a
Cristina Jung <cristijung@gmail.com>
2024-01-10 15:47:12 -0300

    enunciado e img

--- Aula03/script.js
+++ Aula03/script.js
@@ -1,1 +1,6 @@
 //js
+//Samanta é consumidora assídua de uma loja de departamentos, 
+//no dia de hoje ela abusou um pouco nos seus gastos em compras para ela e seu irmão. 
+//Crie um programa que liste os produtos que ela comprou e o quanto ela gastou. 
+//Use as informações contidas na tabela abaixo e, mas abaixo, você encontrará um modelo de saída do programa.
+
