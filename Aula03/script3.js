4a3a80f76973e664a8e272a472382ce878bba14f
Cristina Jung <cristijung@gmail.com>
2024-01-10 16:14:51 -0300

    script3

---
+++ Aula03/script3.js
@@ -0,0 +1,6 @@
+//Braga é estudante de química que tem interesse por programação. 
+//Ele entende que a programação pode ajudá-lo em suas pesquisas e, assim como qualquer 
+//estudante, resolveu fazer alguns testes. O estudante criou um programa que pergunta a 
+//quantidade de carbono de duas substâncias. Para a segunda substância, ele fará um ajuste somando 
+//o número 2 com o valor lido. Depois de feito o ajuste, o programa compara se a quantidade de carbono 
+//da primeira substância é igual ao do segundo e avisa ao usuário se forem iguais.
