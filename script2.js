3622a0e11c8466bc358475e314fbe224ca0f159b
Cristina Jung <cristijung@gmail.com>
2024-01-10 16:02:51 -0300

    script2

---
+++ script2.js
@@ -0,0 +1,5 @@
+//Angélica  teve uma ideia, primeiro, ela apresentou o seguinte trecho de código para seus amigos:
+//Aprendemos que a função prompt() retorna o que é digitado como texto e a função parseInt() converte 
+//um texto em número. Contudo, o que acontece se no lugar da idade for digitado um nome? O valor da idade 
+//será NaN (not a number). Isso faz todo sentido, pois a função parseInt não é capaz de converter um texto 
+//como "Calopsita" para um número.
